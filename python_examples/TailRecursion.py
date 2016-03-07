34040b1a1c265035b384b68ecf6f10c340aecbc0
Alexander Ludkiewicz <alexander.ludkiewicz@polystar.com>
2016-03-07 17:42:47 +0100

    Minor changes and touchups

--- python_examples/TailRecursion.py
+++ python_examples/TailRecursion.py
@@ -1,7 +1,6 @@
 ## Tail recursion in Python. Tail call optimization is not implemented though!
 def call_1000_times(count):
     if count == 1000:
-        print("Hello, world!")
         return True
     else:
         return call_1000_times(count + 1)
