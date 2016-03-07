56cc33f90799681b1ffca69c124bff7c0022e70f
Alexander Ludkiewicz <alexander.ludkiewicz@polystar.com>
2016-03-07 15:23:43 +0100

    Improved comment on Scala closure example

--- scala_examples/makeAdder.scala
+++ scala_examples/makeAdder.scala
@@ -1,5 +1,8 @@
 /**
  * Creating a function that increments from a given (closed-over) starting value, using a closure
+ * Note that the major difference from the same snippet in Javascript and Python comes from Scala
+ * being statically typed. Javascript and Python are dynamically typed, so they don't need all of
+ * the type information Scala does.
  */
 
 object ClosureEx extends App {
